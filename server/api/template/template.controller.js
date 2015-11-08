c3f1376f31abf95a0ebb23121970fb931ff6d0f0
Miller Ren <ren12345@126.com>
2015-11-08 09:43:40 +0800

    template api

---
+++ server/api/template/template.controller.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var _ = require('lodash');
+var Template = require('./template.model');
+
+// Get list of templates
+exports.index = function(req, res) {
+  Template.find(function (err, templates) {
+    if(err) { return handleError(res, err); }
+    return res.status(200).json(templates);
+  });
+};
+
+// Get a single template
+exports.show = function(req, res) {
+  Template.findById(req.params.id, function (err, template) {
+    if(err) { return handleError(res, err); }
+    if(!template) { return res.status(404).send('Not Found'); }
+    return res.json(template);
+  });
+};
+
+// Creates a new template in the DB.
+exports.create = function(req, res) {
+  Template.create(req.body, function(err, template) {
+    if(err) { return handleError(res, err); }
+    return res.status(201).json(template);
+  });
+};
+
+// Updates an existing template in the DB.
+exports.update = function(req, res) {
+  if(req.body._id) { delete req.body._id; }
+  Template.findById(req.params.id, function (err, template) {
+    if (err) { return handleError(res, err); }
+    if(!template) { return res.status(404).send('Not Found'); }
+    var updated = _.merge(template, req.body);
+    updated.save(function (err) {
+      if (err) { return handleError(res, err); }
+      return res.status(200).json(template);
+    });
+  });
+};
+
+// Deletes a template from the DB.
+exports.destroy = function(req, res) {
+  Template.findById(req.params.id, function (err, template) {
+    if(err) { return handleError(res, err); }
+    if(!template) { return res.status(404).send('Not Found'); }
+    template.remove(function(err) {
+      if(err) { return handleError(res, err); }
+      return res.status(204).send('No Content');
+    });
+  });
+};
+
+function handleError(res, err) {
+  return res.status(500).send(err);
+}
