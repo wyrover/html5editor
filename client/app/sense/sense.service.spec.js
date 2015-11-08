0e13ed5c0d42ee1263333a9efec2d11c78313aff
Miller Ren <ren12345@126.com>
2015-11-08 16:03:25 +0800

    sense service

---
+++ client/app/sense/sense.service.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Service: sense', function () {
+
+  // load the service's module
+  beforeEach(module('html5editorApp'));
+
+  // instantiate service
+  var sense;
+  beforeEach(inject(function (_sense_) {
+    sense = _sense_;
+  }));
+
+  it('should do something', function () {
+  });
+
+});
