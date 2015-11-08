88281537412c511bd586471b47c16bbf4b191a79
Miller Ren <ren12345@126.com>
2015-11-08 10:57:50 +0800

    sense api

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -18,6 +18,7 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
+  require('../api/sense/sense.socket').register(socket);
   require('../api/template/template.socket').register(socket);
   require('../api/thing/thing.socket').register(socket);
 }
--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/senses', require('./api/sense'));
   app.use('/api/templates', require('./api/template'));
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
