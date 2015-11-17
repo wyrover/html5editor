b5a3083ac52733f631b1fb95f3384321990f1ff6
Miller Ren <ren12345@126.com>
2015-11-17 10:49:24 +0800

    editor

--- client/app/editor/editor.directive.js
+++ client/app/editor/editor.directive.js
@@ -4,6 +4,9 @@ angular.module('html5editorApp')
   .directive('editor', function () {
     return {
       require: 'ngModel',
+      scope:{
+        sense: '=ngModel'
+      },
       templateUrl: 'app/editor/editor.html',
       restrict: 'EA',
       link: function (scope, element, attrs) {
