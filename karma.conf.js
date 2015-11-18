4d99172c88d6d964d9863b89f5354a9b90d64c4f
Miller Ren <ren12345@126.com>
2015-11-18 10:45:06 +0800

    draggabilly

--- karma.conf.js
+++ karma.conf.js
@@ -24,6 +24,7 @@ module.exports = function(config) {
       'bower_components/lodash/lodash.js',
       'bower_components/angular-socket-io/socket.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'bower_components/angular-async-loader/dist/angular-async-loader.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'client/app/app.js',
