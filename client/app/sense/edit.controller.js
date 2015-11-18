dec187b6e5ebb6684ed54ca974b0fa081bf4b23e
Miller Ren <ren12345@126.com>
2015-11-18 10:56:31 +0800

    init sense

--- client/app/sense/edit.controller.js
+++ client/app/sense/edit.controller.js
@@ -1,6 +1,22 @@
 'use strict';
 
 angular.module('html5editorApp')
-  .controller('SenseEditCtrl', function ($scope, Sense) {
-    $scope.sense = new Sense();
+  .controller('SenseEditCtrl', function ($scope, $state, Sense) {
+    
+    $scope.init = function(id){
+      $scope.sense = new Sense();
+
+      if(id){
+        $scope.sense._id = id;
+        $scope.sense.$get();
+      }
+      else{
+        $scope.sense.contents = [{background:{type:'background'},contents:[]}];
+      }
+
+      $scope.page = $scope.sense.contents[0];
+      $scope.widget = $scope.background;
+    };
+
+    $scope.init($state.params.id);
   });
