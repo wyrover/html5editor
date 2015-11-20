de9e4ba85a044e054c4109608b163cbfb227be6e
Miller Ren <ren12345@126.com>
2015-11-20 20:00:32 +0800

    insert page

--- client/app/editor/editor.pager.directive.js
+++ client/app/editor/editor.pager.directive.js
@@ -10,7 +10,9 @@ angular.module('html5editorApp')
       templateUrl: 'app/editor/editor-pager.html',
       restrict: 'EA',
       controller: function($scope){
-        
+        $scope.insertPage = function($index){
+            $scope.sense.contents.push({background:{type:'background'}})
+        };
       },
       link: function (scope, element, attrs, ngModel) {
         
