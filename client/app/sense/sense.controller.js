214b1fe368bd3af145e57ca4ae04c0415589f999
Miller Ren <ren12345@126.com>
2015-11-21 00:31:33 +0800

    sense delte

--- client/app/sense/sense.controller.js
+++ client/app/sense/sense.controller.js
@@ -3,4 +3,9 @@
 angular.module('html5editorApp')
   .controller('SenseCtrl', function ($scope, Sense) {
     $scope.senses = Sense.query();
+    $scope.remove = function(index){
+      $scope.senses[index].$remove(function(){
+        $scope.senses.splice(index,1);
+      });
+    };
   });
