16ba4967e4bc50fcd7cf15338e026895008e8d7e
Miller Ren <ren12345@126.com>
2015-11-21 23:19:20 +0800

    widget drag

--- client/app/editor/stage.directive.js
+++ client/app/editor/stage.directive.js
@@ -16,8 +16,8 @@ angular.module('html5editorApp')
           y = widget.top;
         };
         $scope.onPanMove = function($event, widget){
-          widget.left = x + $event.deltaX;
-          widget.top = y + $event.deltaY;
+          widget.left = parseInt(x) + $event.deltaX;
+          widget.top = parseInt(y) + $event.deltaY;
         };
       },
       link: function (scope, element, attrs, ngModel) {
