b2009691d31bf3da593956e8f701a33cdefdf974
Miller Ren <ren12345@126.com>
2015-11-21 22:58:59 +0800

    editor wdiget

---
+++ client/app/editor/editor.widget.directive.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('html5editorApp')
+  .directive('editorWidget', function () {
+    return {
+      require: 'ngModel',
+      scope:{
+        widget: '=ngModel'
+      },
+      templateUrl: 'app/editor/editor-widget.html',
+      restrict: 'EA',
+      controller: function($scope){
+        
+      },
+      link: function (scope, element, attrs, ngModel) {
+        
+      }
+    };
+  });
