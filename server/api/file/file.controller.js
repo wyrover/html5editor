0e9b6ca1807258ff061969ecefbb10538e6efb5e
Miller Ren <ren12345@126.com>
2015-11-23 11:17:44 +0800

    files endpoint

---
+++ server/api/file/file.controller.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var _ = require('lodash');
+var File = require('./file.model');
+
+// Get list of files
+exports.index = function(req, res) {
+  File.find(function (err, files) {
+    if(err) { return handleError(res, err); }
+    return res.status(200).json(files);
+  });
+};
+
+// Get a single file
+exports.show = function(req, res) {
+  File.findById(req.params.id, function (err, file) {
+    if(err) { return handleError(res, err); }
+    if(!file) { return res.status(404).send('Not Found'); }
+    return res.json(file);
+  });
+};
+
+// Creates a new file in the DB.
+exports.create = function(req, res) {
+  File.create(req.body, function(err, file) {
+    if(err) { return handleError(res, err); }
+    return res.status(201).json(file);
+  });
+};
+
+// Updates an existing file in the DB.
+exports.update = function(req, res) {
+  if(req.body._id) { delete req.body._id; }
+  File.findById(req.params.id, function (err, file) {
+    if (err) { return handleError(res, err); }
+    if(!file) { return res.status(404).send('Not Found'); }
+    var updated = _.merge(file, req.body);
+    updated.save(function (err) {
+      if (err) { return handleError(res, err); }
+      return res.status(200).json(file);
+    });
+  });
+};
+
+// Deletes a file from the DB.
+exports.destroy = function(req, res) {
+  File.findById(req.params.id, function (err, file) {
+    if(err) { return handleError(res, err); }
+    if(!file) { return res.status(404).send('Not Found'); }
+    file.remove(function(err) {
+      if(err) { return handleError(res, err); }
+      return res.status(204).send('No Content');
+    });
+  });
+};
+
+function handleError(res, err) {
+  return res.status(500).send(err);
+}
