3658eda892f6a1f1ece8431408dfb24e3075f02b
Miller Ren <ren12345@126.com>
2015-11-23 19:20:05 +0800

    watch index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,7 +99,8 @@ module.exports = function (grunt) {
           '{.tmp,<%= yeoman.client %>}/{app,components}/**/*.js',
           '!{.tmp,<%= yeoman.client %>}{app,components}/**/*.spec.js',
           '!{.tmp,<%= yeoman.client %>}/{app,components}/**/*.mock.js',
-          '<%= yeoman.client %>/assets/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}'
+          '<%= yeoman.client %>/assets/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= yeoman.client %>/*.html'
         ],
         options: {
           livereload: true
