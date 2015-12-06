c724d40f8807ac49a660802792a6a96d9f459bdd
Miller Ren <ren12345@126.com>
2015-12-06 16:58:24 +0800

    modal

--- client/app/sense/sense.controller.js
+++ client/app/sense/sense.controller.js
@@ -4,14 +4,12 @@ angular.module('html5editorApp')
   .controller('SenseCtrl', function ($scope, $modal, Sense, Modal) {
     $scope.senses = Sense.query();
     $scope.remove = function(index){
-      Modal({
+      Modal.confirm({
         title:'确认删除？'
-      }, function(){
-        console.log(arguments)
-      });
-      return;
-      $scope.senses[index].$remove(function(){
-        $scope.senses.splice(index,1);
+      }).result.then(function(){
+        $scope.senses[index].$remove(function(){
+          $scope.senses.splice(index,1);
+        });
       });
     };
   });
