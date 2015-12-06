7fa6d11388ea98c454ae42c0b42cc8959db7e4a1
Miller Ren <ren12345@126.com>
2015-12-06 11:59:19 +0800

    init sense page

--- client/app/editor/editor.directive.js
+++ client/app/editor/editor.directive.js
@@ -11,7 +11,11 @@ angular.module('html5editorApp')
       restrict: 'EA',
       controller: function($scope, EditorWidget){
         $scope.init = function(){
-          $scope.page = $scope.sense.contents[0];
+          var active = 0;
+          angular.forEach($scope.sense.contents, function(item, index){
+            if(item.active) active = index;
+          });
+          $scope.page = $scope.sense.contents[active];
           EditorWidget.widget = $scope.page.background;
         };
         if($scope.sense._id){
