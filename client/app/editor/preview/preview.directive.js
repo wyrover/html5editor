5220ffe21653d6547c45ed86e559fcfe411f7cc4
Miller Ren <ren12345@126.com>
2015-12-09 16:04:35 +0800

    preview

--- client/app/editor/preview/preview.directive.js
+++ client/app/editor/preview/preview.directive.js
@@ -3,8 +3,14 @@
 angular.module('html5editorApp')
   .directive('editorPreview', function () {
     return {
+      scope:{
+        sense:'=ngModel'
+      },
       templateUrl: 'app/editor/preview/preview.html',
       restrict: 'EA',
+      controller: function($scope){
+        //$scope.page = $scope.sense.contents[0];
+      },
       link: function (scope, element, attrs) {
       }
     };
