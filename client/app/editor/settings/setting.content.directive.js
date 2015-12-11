9a413e24ac76f299fbfb40f6f79cc3f7896a3996
Miller Ren <ren12345@126.com>
2015-12-11 15:21:13 +0800

    form settings template

--- client/app/editor/settings/setting.content.directive.js
+++ client/app/editor/settings/setting.content.directive.js
@@ -11,6 +11,16 @@ angular.module('html5editorApp')
       restrict: 'EA',
       controller: function($scope, Upload, EditorWidget, FileModal){
         $scope.widget = EditorWidget;
+
+        $scope.fieldTypes = [
+          {name:'输入框',type:'input'},
+          {name:'文本框',type:'textarea'},
+          {name:'下拉框',type:'select'},
+          {name:'单选按钮',type:'radio'},
+          {name:'多选按钮',type:'checkbox'},
+        ];
+
+        $scope.fieldType = $scope.widget.widget.fieldType||'input';
         
         $scope.selectBackground = function(){
           FileModal().result.then(function(file){
