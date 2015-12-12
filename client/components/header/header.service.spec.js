aa75c21a1f233b10952c2bb3f47bcedd76c57149
Miller Ren <ren12345@126.com>
2015-12-12 15:18:03 +0800

    header range

---
+++ client/components/header/header.service.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Service: header', function () {
+
+  // load the service's module
+  beforeEach(module('html5editorApp'));
+
+  // instantiate service
+  var header;
+  beforeEach(inject(function (_header_) {
+    header = _header_;
+  }));
+
+  it('should do something', function () {
+  });
+
+});
