f707bb0b2af7e00315f12eea2e5567d5c776e671
Miller Ren <ren12345@126.com>
2015-12-18 17:38:48 +0800

    sense remove

--- client/app/sense/sense.controller.js
+++ client/app/sense/sense.controller.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('html5editorApp')
-  .controller('SenseCtrl', function ($scope, $modal, $state, Sense, TemplateModal) {
+  .controller('SenseCtrl', function ($scope, $modal, $state, Sense, Modal, TemplateModal) {
     $scope.senses = Sense.query();
 
     $scope.remove = function(index){
       Modal.confirm({
-        title:'确认删除？'
+        title:'确认删除？',
+        value:'删除后将无法恢复。'
       }).result.then(function(){
         $scope.senses[index].$remove(function(){
           $scope.senses.splice(index,1);
