8bf894c07e1ea91352b6250089cb098971755bbe
Miller Ren <MillerRen@github.com>
2016-01-06 18:54:43 +0800

    notice route

--- client/app/notice/notice.js
+++ client/app/notice/notice.js
@@ -5,7 +5,32 @@ angular.module('html5editorApp')
     $stateProvider
       .state('notice', {
         url: '/notice',
+        abstract: true,
+        views: {
+          'nav':{
+            templateUrl: 'components/navbar/navbar.html'
+          },
+          '':{
+            template: '<div ui-view="" class="container"></div>'
+          },
+          'footer':{
+            templateUrl: 'components/footer/footer.html'
+          }
+        }
+      })
+      .state('notice.index', {
+        url: '',
         templateUrl: 'app/notice/notice.html',
         controller: 'NoticeCtrl'
-      });
+      })
+      .state('notice.new', {
+        url: '/new',
+        templateUrl: 'app/notice/edit.html',
+        controller: 'NoticeEditCtrl'
+      })
+      .state('notice.edit', {
+        url: '/edit/:id',
+        templateUrl: 'app/notice/edit.html',
+        controller: 'NoticeEditCtrl'
+      });;
   });
