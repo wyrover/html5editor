c128fac30cd7b18040b770ffdbf606aae33553eb
MillerRen <MillerRen@github.com>
2016-03-23 12:06:19 +0800

    content type

--- server/files/file.controller.js
+++ server/files/file.controller.js
@@ -7,12 +7,12 @@ var gfs = Grid(mongoose.connection.db, mongoose.mongo);
 
 // Get a single file
 exports.show = function(req, res) {
-  gfs.exist({_id:req.params.id}, function(err, found){
+  gfs.findOne({_id:req.params.id}, function(err, found){
     if (err) return handleError(res, err);
     if(!found){
       return res.status(404).send('');
     }
-    console.log(found)
+    res.set('Content-Type', found.contentType);
     var stream = gfs.createReadStream({_id:req.params.id});
     stream.on('error', function(err){
       res.status(404, err)
